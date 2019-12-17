cb10f1d3ffd718207d48f2f3e2131e6bb41a86b9
yaamanetaisei <b1936670@planet.kanazawa-it.ac.jp>
2019-12-17 19:06:18 +0900

    保存食一覧のファイルを追加

---
+++ app/src/main/java/com/example/kanazawaapp_2019/PreservedFoodsListActivity.kt
@@ -0,0 +1,14 @@
+package com.example.kanazawaapp_2019
+
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PreservedFoodsListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_preserved_foods_list)
+    }
+}
