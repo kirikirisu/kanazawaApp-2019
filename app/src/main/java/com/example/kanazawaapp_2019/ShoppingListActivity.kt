9c875980d7eeb57967c038f2a1b81ae725742ae5
Shinbo-Rina <b1915499@planet.kanazawa-it.ac.jp>
2020-01-12 06:51:42 +0900

    footerによる画面遷移の実装

--- app/src/main/java/com/example/kanazawaapp_2019/ShoppingListActivity.kt
+++ app/src/main/java/com/example/kanazawaapp_2019/ShoppingListActivity.kt
@@ -3,6 +3,7 @@ package com.example.kanazawaapp_2019
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.tabs.TabLayout
 import kotlinx.android.synthetic.main.activity_shoppinglist.*
 
 class ShoppingListActivity : AppCompatActivity() {
@@ -16,6 +17,31 @@ class ShoppingListActivity : AppCompatActivity() {
             val intent = Intent(application, FoodAdditionActivity::class.java)
             startActivity(intent)
         }
+
+        //footerの実装
+        shoppingListTabLayout.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                var position = shoppingListTabLayout.selectedTabPosition
+                if (position == 0) {
+                    val intent = Intent(application, PreservedFoodsListActivity::class.java)
+                    startActivities(arrayOf(intent))
+                }else {
+                    shoppingListView.setSelection(0)
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab) {}
+
+            override fun onTabReselected(tab: TabLayout.Tab) {
+                var position = shoppingListTabLayout.selectedTabPosition
+                if (position == 0) {
+                    val intent = Intent(application, PreservedFoodsListActivity::class.java)
+                    startActivities(arrayOf(intent))
+                }else {
+                    shoppingListView.setSelection(0)
+                }
+            }
+        })
 
     }
 }
