7d5b9efa7e0b78e33acd3a2a66612f50030c8fc3
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2020-01-18 14:45:25 +0900

    app/src/main/AndroidManifest.xml

---
+++ app/src/main/java/com/example/kanazawaapp_2019/SqlSampleActivity.kt
@@ -0,0 +1,13 @@
+package com.example.kanazawaapp_2019
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SqlSampleActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sql_sample)
+    }
+
+}
