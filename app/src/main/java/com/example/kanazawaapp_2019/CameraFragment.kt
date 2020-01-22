54080ca99e88615ef2fe3eb716bd4c351cda056c
daikimare <b1840702@planet.kanazawa-it.ac.jp>
2020-01-22 18:14:27 +0900

    fragment内のボタンの挙動確認と設定

--- app/src/main/java/com/example/kanazawaapp_2019/CameraFragment.kt
+++ app/src/main/java/com/example/kanazawaapp_2019/CameraFragment.kt
@@ -2,10 +2,12 @@ package com.example.kanazawaapp_2019
 
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ListView
 
 /**
@@ -22,4 +24,24 @@ class CameraFragment : Fragment() {
 
     }
 
+    override fun onStart() {
+        super.onStart()
+        val BarcodeButton: Button = activity!!.findViewById(R.id.BarcodeReading)
+        val CameraButton: Button = activity!!.findViewById(R.id.PictureTaking)
+        val CameraRollButton : Button = activity!!.findViewById(R.id.PictureUsing)
+        BarcodeButton.setOnClickListener{
+            Log.d("追加","バーコード")
+            clickBarcode()
+        }
+        CameraButton.setOnClickListener{
+            Log.d("追加","カメラ")
+        }
+        CameraRollButton.setOnClickListener{
+            Log.d("追加","カメラロール")
+        }
+    }
+    //バーコードボタンの挙動
+    fun clickBarcode(){
+
+    }
 }
