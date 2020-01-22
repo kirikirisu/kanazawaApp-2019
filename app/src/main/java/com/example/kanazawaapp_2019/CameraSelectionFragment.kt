eab79a82d0f1d2bbfef9e5a34bd1985dd423a853
daikimare <b1840702@planet.kanazawa-it.ac.jp>
2020-01-22 20:44:41 +0900

    カメラの起動

--- app/src/main/java/com/example/kanazawaapp_2019/CameraSelectionFragment.kt
+++ app/src/main/java/com/example/kanazawaapp_2019/CameraSelectionFragment.kt
@@ -3,20 +3,22 @@ package com.example.kanazawaapp_2019
 
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.ListView
+import android.widget.Toast
 import java.lang.Exception
 
 /**
  * A simple [Fragment] subclass.
  */
-class CameraFragment : Fragment() {
+class CameraSelectionFragment : Fragment() {
 val READRE_REQUEST_CODE : Int =42
 
     override fun onCreateView(
@@ -24,7 +26,7 @@ val READRE_REQUEST_CODE : Int =42
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_camera, container, false)
+        return inflater.inflate(R.layout.fragment_camera_selection, container, false)
 
     }
 
@@ -39,6 +41,7 @@ val READRE_REQUEST_CODE : Int =42
         }
         CameraButton.setOnClickListener{
             Log.d("追加","カメラ")
+            clickCamera()
         }
         CameraRollButton.setOnClickListener{
             Log.d("追加","カメラロール")
@@ -49,7 +52,16 @@ val READRE_REQUEST_CODE : Int =42
     fun clickBarcode(){
 
     }
-    //
+
+    //カメラボタンの挙動
+    fun clickCamera(){
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also{ takePictureIntent ->
+            takePictureIntent.resolveActivity(this.context!!.packageManager)?.also {
+                startActivityForResult(takePictureIntent,1)
+            }?:Toast.makeText(this.context,"カメラを扱うアプリがありません",Toast.LENGTH_LONG).show()
+        }
+    }
+    //カメラロールボタンの挙動
     fun clickCameraRoll(){
         val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
             addCategory(Intent.CATEGORY_OPENABLE)
