e50d69aa7b0af0de317ee4d1294ce143aa6532d5
yaamanetaisei <b1936670@planet.kanazawa-it.ac.jp>
2020-01-24 22:57:48 +0900

    TabBarに変更、タブのデザイン変更

--- app/src/main/java/com/example/kanazawaapp_2019/PagerAdaptorFoodList.kt
+++ app/src/main/java/com/example/kanazawaapp_2019/PagerAdaptorFoodList.kt
@@ -6,6 +6,7 @@ import StapleFoodFragmentActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import com.google.android.material.tabs.TabLayout
 
 val tabsFoodList = 3
 // number of tabs
