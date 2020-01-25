bbc3e7e260c9e1b7103cf7caf817e6522ab733f0
KanaiYuta <b1947571@planet.kanazawa-it.ac.jp>
2020-01-25 15:27:30 +0900

    ユーザーの入力した値に対応させた

--- app/src/main/java/com/example/kanazawaapp_2019/InitialNumberSettingActivity.kt
+++ app/src/main/java/com/example/kanazawaapp_2019/InitialNumberSettingActivity.kt
@@ -2,7 +2,8 @@ package com.example.kanazawaapp_2019
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class InitialNumberSettingActivity : AppCompatActivity() {
@@ -10,6 +11,29 @@ class InitialNumberSettingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_initial_number_setting)
+
+        val nextButton = findViewById<Button>(R.id.moveNextBtn)
+
+        nextButton.setOnClickListener{
+            var isValid = true
+
+            val humanNumberText = findViewById<EditText>(R.id.humanNumber)
+            val number = humanNumberText.text.toString()
+
+            if (number.isEmpty()) {
+                humanNumberText.error = "入力してください"
+                isValid = false
+            }
+
+            if (isValid) {
+                val humanNumber = Integer.parseInt(number)
+
+                val intent = Intent(this, InitialNotificationSettingActivity::class.java)
+                intent.putExtra("humanNumber",humanNumber)
+                startActivity(intent)
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+            }
+        }
     }
 
     override fun finish() {
@@ -17,9 +41,4 @@ class InitialNumberSettingActivity : AppCompatActivity() {
         overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right)
     }
 
-    fun toNext(view: View) {
-        val intent = Intent(this, InitialNotificationSettingActivity::class.java)
-        startActivity(intent)
-        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
-    }
 }
