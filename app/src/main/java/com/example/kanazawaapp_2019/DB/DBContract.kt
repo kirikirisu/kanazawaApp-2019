0ecc9bc14aec5bd5e02c3da7d11e29e0dfadc14b
shxn6934 <b1612851@planet.kanazawa-it.ac.jp>
2020-02-03 20:08:54 +0900

    保存食品DB雛形追加

--- app/src/main/java/com/example/kanazawaapp_2019/DB/DBContract.kt
+++ app/src/main/java/com/example/kanazawaapp_2019/DB/DBContract.kt
@@ -1,6 +1,7 @@
 import android.provider.BaseColumns
 
 object DBContract {
+    // 保存食一覧's table
     object PreservedFoodEntry: BaseColumns {
         const val TABLE_NAME = "PreservedFoodTable"
         const val FOOD_NAME = "foodName"
@@ -9,11 +10,14 @@ object DBContract {
         const val QUANTITY = "quantity"
         const val CREATED_AT = "createdAt"
         const val COMMERICAL = "commercial"
+        const val FOOD_TYPE = "foodType"
     }
 
-//    object ShoppingListEntyty: BaseColumns {
-//        const val TABLE_NAME = "ShoppingListTable"
-//        const val FOOD_NAME = "foodName"
-//        const val CREATED_AT = "createdAt"
-//    }
+    // 買い物リスト's table
+    object ShoppingListEntyty: BaseColumns {
+        const val TABLE_NAME = "ShoppingListTable"
+        const val FOOD_NAME = "foodName"
+        const val CREATED_AT = "createdAt"
+        const val USED = "used"
+    }
 }
--- app/src/main/java/com/example/kanazawaapp_2019/DB/PreservedFoodDBHelper.kt
+++ app/src/main/java/com/example/kanazawaapp_2019/DB/PreservedFoodDBHelper.kt
@@ -1,12 +1,20 @@
 package com.example.kanazawaapp_2019.DB
 
+import android.content.ContentValues
 import android.provider.BaseColumns
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import android.util.Log
 
 internal class PreservedFoodDBHelper(content: Context) :
     SQLiteOpenHelper(content, DATABASE_NAME, null, DATABASE_VERSION) {
+
+    companion object {
+        // If you change the database schema, you must increment the database version.
+        private val DATABASE_NAME: String = "Food.db"
+        const val DATABASE_VERSION = 1
+    }
 
     private val SQL_CREATE_ENTRIES =
         "create table if not exists ${DBContract.PreservedFoodEntry.TABLE_NAME} (" +
@@ -16,7 +24,8 @@ internal class PreservedFoodDBHelper(content: Context) :
                 "${DBContract.PreservedFoodEntry.STORAGE_LOCATION} text," +
                 "${DBContract.PreservedFoodEntry.QUANTITY} integer," +
                 "${DBContract.PreservedFoodEntry.CREATED_AT} text not null default (datetime('now', 'localtime'))," +
-                "${DBContract.PreservedFoodEntry.COMMERICAL} text);"
+                "${DBContract.PreservedFoodEntry.COMMERICAL} text);" +
+                "${DBContract.PreservedFoodEntry.FOOD_TYPE} integer"
 
     private val SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS ${DBContract.PreservedFoodEntry.TABLE_NAME}"
 
@@ -31,9 +40,76 @@ internal class PreservedFoodDBHelper(content: Context) :
         onCreate(db)
     }
 
-    companion object {
-        // If you change the database schema, you must increment the database version.
-        private val DATABASE_NAME: String = "Food.db"
-        const val DATABASE_VERSION = 1
+//    fun getCreatedAt(): String {
+//        val date = Date()
+//        val format = SimpleDateFormat("yyyyMMddHHmmss", Locale.getDefault())
+//        createdAt = format.format(date)
+//        return ""
+//    }
+
+    fun insertPreservedFoods(foodName: String, deadline: String, storageLocation: String, quantity: Int, createdAt: String, commercial: String, foodType: Int){
+        try{
+            val db = this.writableDatabase
+            val values = ContentValues().apply {
+                put("${DBContract.PreservedFoodEntry.FOOD_NAME}", foodName)
+                put("${DBContract.PreservedFoodEntry.DEADLINE}", deadline)
+                put("${DBContract.PreservedFoodEntry.STORAGE_LOCATION}", storageLocation)
+                put("${DBContract.PreservedFoodEntry.QUANTITY}", quantity)
+                put("${DBContract.PreservedFoodEntry.CREATED_AT}", createdAt)
+                put("${DBContract.PreservedFoodEntry.COMMERICAL}", commercial)
+                put("${DBContract.PreservedFoodEntry.FOOD_TYPE}", foodType)
+            }
+            db.insert(DBContract.PreservedFoodEntry.TABLE_NAME, null, values)
+            db.close()
+        }catch(exception: Exception){
+            Log.e("DB Insert Error", exception.toString())
+        }
     }
+
+//    private fun selectPreservedFoods(){
+//        try{
+//            val PreservedFoodIds = mutableListOf<Long>()
+//            val FoodNames = mutableListOf<Long>()
+//            val Deadlines = mutableListOf<Long>()
+//            val StorageLocations = mutableListOf<Long>()
+//            val Quantities = mutableListOf<Long>()
+//            val arrayListCreatedAt = mutableListOf<Long>()
+//            val Commercials = mutableListOf<Long>()
+//
+//            val db = this.readableDatabase
+//            val sql = "select * from ${DBContract.PreservedFoodEntry.TABLE_NAME}"
+//            val cursor = db.rawQuery(sql, null)
+//
+//            with(cursor) {
+//                if (count > 0) {
+//                    moveToFirst()
+//                    while (moveToNext()) {
+//                        val PreservedFoodId = getLong(getColumnIndexOrThrow(BaseColumns._ID))
+//                        val FoodName =
+//                            getLong(getColumnIndexOrThrow(DBContract.PreservedFoodEntry.FOOD_NAME))
+//                        val Deadline =
+//                            getLong(getColumnIndexOrThrow(DBContract.PreservedFoodEntry.DEADLINE))
+//                        val StorageLocation =
+//                            getLong(getColumnIndexOrThrow(DBContract.PreservedFoodEntry.STORAGE_LOCATION))
+//                        val Quantity =
+//                            getLong(getColumnIndexOrThrow(DBContract.PreservedFoodEntry.QUANTITY))
+//                        val CreatedAt =
+//                            getLong(getColumnIndexOrThrow(DBContract.PreservedFoodEntry.CREATED_AT))
+//                        val Commercial =
+//                            getLong(getColumnIndexOrThrow(DBContract.PreservedFoodEntry.COMMERICAL))
+//
+//                        PreservedFoodIds.add(PreservedFoodId)
+//                        FoodNames.add(FoodName)
+//                        Deadlines.add(Deadline)
+//                        StorageLocations.add(StorageLocation)
+//                        Quantities.add(Quantity)
+//                        arrayListCreatedAt.add(CreatedAt)
+//                        Commercials.add(Commercial)
+//                    }
+//                }
+//            }
+//        }catch(exeption: Exception) {
+//            Log.e("Log", exeption.toString())
+//        }
+//    }
 }
